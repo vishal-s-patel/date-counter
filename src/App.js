6f19fdf8b8e7656b1ce8ab46e4031686f9a58f0d
vishal-s-patel <vishalpatel1002@yahoo.com>
2024-07-07 16:20:32 +0530

    feat: controlled elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+  let dt = new Date();
+  dt.setDate(dt.getDate() + count);
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(parseInt(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setCount((c) => c - step);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          &minus;
+        </button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button
+          onClick={() => {
+            setCount((c) => c + step);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <p>
+        {count === 0
+          ? "Today is"
+          : count > 0
+          ? `${count} days from today is`
+          : `${Math.abs(count)} days ago was`}{" "}
+        {dt.toDateString()}
+      </p>
+
+      {(count !== 0 || step !== 1) && (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      )}
     </div>
   );
 }
